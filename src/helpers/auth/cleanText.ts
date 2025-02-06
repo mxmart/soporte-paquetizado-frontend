f0d0369e963bd86d179be10be3f24b5d41cd7e58
César Pérez <asgarper11@gmail.com>
2025-02-06 09:03:48 -0700

    changes

--- src/helpers/auth/cleanText.ts
+++ src/helpers/auth/cleanText.ts
@@ -1,9 +1,11 @@
 export function cleanText(input: string): string {
     // Utilizar una expresión regular para eliminar la fecha y hora
     const sinFecha = input.replace(/\d{1,2}\/\d{1,2}\/\d{4}, \d{2}:\d{2}:\d{2}/, '');
+    console.log({ sinFecha })
     
     // Eliminar las comas sobrantes
     const resultado = sinFecha.replace(/,/g, '').trim();
+    console.log({ resultado })
     
     return resultado;
   }
